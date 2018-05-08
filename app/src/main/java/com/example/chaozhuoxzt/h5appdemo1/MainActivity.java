dc798b59add9c791b175a376182a53691ddc479d
xiezhongtian <xiezhongtian@phoenixos.com>
2018-05-08 10:31:03 +0800

    android external class implement onClick

--- app/src/main/java/com/example/chaozhuoxzt/h5appdemo1/MainActivity.java
+++ app/src/main/java/com/example/chaozhuoxzt/h5appdemo1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button bt1;
     private Button bt2;
+    private Button imgbt;
     private Context context;
     private TextView textView;
 
@@ -26,18 +28,20 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView1);
 
         //jump by startActivity
-        bt1.setOnClickListener(new View.OnClickListener() {
+        bt1.setOnClickListener(new MyOnClickListener() {
             @Override
             public void onClick(View v) {
+                super.onClick(v);//call base class onClick
                 Intent intent = new Intent(context,SActivity.class);
                 startActivity(intent);
             }
         });
 
         //jump by startActivityForResult
-        bt2.setOnClickListener(new View.OnClickListener() {
+        bt2.setOnClickListener(new MyOnClickListener() {
             @Override
             public void onClick(View v) {
+                super.onClick(v);
                 Intent intent = new Intent(context,SActivity.class);
                 startActivityForResult(intent,1);
             }
@@ -52,5 +56,14 @@ public class MainActivity extends AppCompatActivity {
             String content = data.getStringExtra("data");
             textView.setText(content);
         }
+    }
+}
+
+class MyOnClickListener implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+        Log.i("tag","父类的Onclick方法");
+        v.setAlpha(0.5f);
     }
 }
